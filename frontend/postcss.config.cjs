f6ed751903d247dde04548e1e57d425a48195434
gianlucavedovato-akqa <gianluca.vedovato@akqa.com>
2025-04-24 17:18:13 +0200

    Modernize UI with PrimeVue components and fix build issues

---
+++ frontend/postcss.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: {
+    // Since we're using PrimeVue now, we don't need Tailwind
+    // But we'll keep autoprefixer for better browser compatibility
+    autoprefixer: {}
+  }
+}
